1892547ae442efd35f5ee067335f6cda43dced81
avchen <anthony.chen813@gmail.com>
2022-01-07 16:51:53 -0500

    Added HTML interface with basic styles to simulate a rock paper scissors app. Updated JS for DOM manipulation.

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-let numberOfPlayerWins;
-let numberOfComputerWins;
+let numberOfWins = 0;
+let numberOfLosses = 0;
+let numberOfTies = 0;
+let numberOfRounds = 0;
+const buttons = document.querySelectorAll('.select-btn');
+const instructions = document.querySelector('#instruction-text');
+const winCount = document.querySelector('.win-text');
+const lossCount = document.querySelector('.loss-text');
+const tieCount = document.querySelector('.tie-text');
+const resetButton = document.querySelector('#reset-button');
+const gameLogTableBody = document.querySelector('tbody');
 
+buttons.forEach(button => {
+    button.addEventListener('click', playRound);
+});
 
 function computerPlay () {
     let computerValue = Math.floor(Math.random() * 3 + 1);
@@ -14,76 +26,81 @@ function computerPlay () {
     }
 }
 
+function capitalizeString(string) {
+    return string[0].toUpperCase() + string.substring(1);
+}
+
+function updateScoreboard () {
+    winCount.textContent = `Wins: ${numberOfWins}`;
+    lossCount.textContent = `Losses: ${numberOfLosses}`;
+    tieCount.textContent = `Ties: ${numberOfTies}`;
+}
+
+function updateGameLog (playerSelect, cpuSelect, result) {
+    gameLogTableBody.innerHTML += `<tr><td>${playerSelect}</td><td>${cpuSelect}</td><td>${result}</td></tr>`;
+}
+
+resetButton.addEventListener('click', () => {
+    numberOfWins = 0;
+    numberOfLosses = 0;
+    numberOfTies = 0;
+    numberOfRounds = 0;
+    instructions.textContent = `Welcome! You'll be playing "Rock, Paper, Scissors" against a computer. To begin, select either rock, paper, or scissors. Your running score against the computer will be recorded.`;
+    gameLogTableBody.innerHTML = '';
+    updateScoreboard();
+});
+
+updateScoreboard();
+
+
 function playRound(playerSelection, computerSelection) {
+    playerSelection = this.id;
     computerSelection = computerPlay();
+    let roundResult = '';
     if (playerSelection === 'rock') {
         if (computerSelection === 'paper') {
-            console.log(`You lose. You selected ${playerSelection} and the computer chose ${computerSelection}.`);
-            numberOfComputerWins += 1;
+            instructions.textContent = `You lose. You selected ${playerSelection} and the computer chose ${computerSelection}.`;
+            numberOfLosses += 1;
+            roundResult = 'Loss';
         } else if (computerSelection === 'scissors') {
-            console.log(`You win! You selected ${playerSelection} and the computer chose ${computerSelection}.`);
-            numberOfPlayerWins += 1;
+            instructions.textContent = (`You win! You selected ${playerSelection} and the computer chose ${computerSelection}.`);
+            numberOfWins += 1;
+            roundResult = 'Win';
         } else {
-            console.log(`It was a tie. You both selected ${playerSelection}.`);
+            instructions.textContent = (`It was a tie. You both selected ${playerSelection}.`);
+            numberOfTies += 1;
+            roundResult = 'Tie';
         }
     } else if (playerSelection === 'paper') {
         if (computerSelection === 'scissors') {
-            console.log(`You lose. You selected ${playerSelection} and the computer chose ${computerSelection}.`);
-            numberOfComputerWins += 1;
+            instructions.textContent = (`You lose. You selected ${playerSelection} and the computer chose ${computerSelection}.`);
+            numberOfLosses += 1;
+            roundResult = 'Loss';
         } else if (computerSelection === 'rock') {
-            console.log(`You win! You selected ${playerSelection} and the computer chose ${computerSelection}.`);
-            numberOfPlayerWins += 1;
+            instructions.textContent = (`You win! You selected ${playerSelection} and the computer chose ${computerSelection}.`);
+            numberOfWins += 1;
+            roundResult = 'Win';
         } else {
-            console.log(`It was a tie. You both selected ${playerSelection}.`);
+            instructions.textContent = (`It was a tie. You both selected ${playerSelection}.`);
+            numberOfTies += 1;
+            roundResult = 'Tie';
         }
     } else {
         if (computerSelection === 'rock') {
-            console.log(`You lose. You selected ${playerSelection} and the computer chose ${computerSelection}.`);
-            numberOfComputerWins += 1;
+            instructions.textContent = (`You lose. You selected ${playerSelection} and the computer chose ${computerSelection}.`);
+            numberOfLosses += 1;
+            roundResult = 'Loss';
         } else if (computerSelection === 'paper') {
-            console.log(`You win! You selected ${playerSelection} and the computer chose ${computerSelection}.`);
-            numberOfPlayerWins += 1;
+            instructions.textContent = (`You win! You selected ${playerSelection} and the computer chose ${computerSelection}.`);
+            numberOfWins += 1;
+            roundResult = 'Win';
         } else {
-            console.log(`It was a tie. You both selected ${playerSelection}.`);
+            instructions.textContent = (`It was a tie. You both selected ${playerSelection}.`);
+            numberOfTies += 1;
+            roundResult = 'Tie';
         }
     }
+    updateScoreboard();
+    updateGameLog(capitalizeString(playerSelection), capitalizeString(computerSelection), roundResult);
 }
 
-function game(rounds) {
-    numberOfPlayerWins = 0;
-    numberOfComputerWins = 0;
-    for (roundNumber = 0; roundNumber < rounds; roundNumber++) {
-        let playerInput = prompt("What is your selection?");
-        if (playerInput === null) {
-            alert("Canceled");
-            break;
-        };
-        let formattedPlayerInput = playerInput.trim().toLowerCase();
-        let computerSelection = computerPlay();
-        playRound(formattedPlayerInput, computerSelection);
-    }
-
-    let numberOfTies = roundNumber - numberOfComputerWins - numberOfPlayerWins;
-
-    if (numberOfPlayerWins > numberOfComputerWins) {
-        alert(`You won overall with a total of ${numberOfPlayerWins} wins, ${numberOfComputerWins} losses, and ${numberOfTies} ties!`);
-    } else if (numberOfPlayerWins < numberOfComputerWins) {
-        alert(`You lost overall with a total of ${numberOfPlayerWins} wins, ${numberOfComputerWins} losses, and ${numberOfTies} ties.`);
-    } else if (roundNumber > 0 && numberOfPlayerWins === numberOfComputerWins) {
-        alert(`Wow! You tied, each having won ${numberOfComputerWins} times!`);
-    }
-}
-
-// function validatePlayerInput() {
-//     let playerInput;
-    
-//     while (playerInput !== 'rock' || playerInput !== 'paper' || playerInput!== 'scissors') {
-//     playerInput = prompt("What is your selection?");
-//     if (playerInput === null) {
-//         alert("Canceled");
-//         return;
-//     }
-//     playerInput = playerInput.trim().toLowerCase();
-//     return playerInput;
-//     } 
-// }
